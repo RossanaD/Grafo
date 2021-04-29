fc1b3261849cd02b55b49eb37c0bc2ec0f4627a2
Rossana Dullius <67810701+RossanaD@users.noreply.github.com>
2021-04-29 00:11:17 -0300

    Add files via upload

---
+++ GrafosSPOJ/GrafosSPOJ/src/SPOJNatureza/Aresta.java
@@ -0,0 +1,28 @@
+package SPOJNatureza;
+
+public class Aresta<T> {
+	private Vertice<T> inicio;
+	private Vertice<T> fim;
+	
+	public Aresta(Vertice<T> inicio, Vertice<T> fim) {
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Vertice<T> getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Vertice<T> inicio) {
+		this.inicio = inicio;
+	}
+
+	public Vertice<T> getFim() {
+		return fim;
+	}
+
+	public void setFim(Vertice<T> fim) {
+		this.fim = fim;
+	}
+	
+}
